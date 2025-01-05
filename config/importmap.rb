e5d99c841aeae7d4302f48c72cd8fd003b1c0f8f
yknli <augustus0613@hotmail.com>
2025-01-05 17:19:46 +0800

    chore: pin rails/ujs from url

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,4 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 # Note: Everything above was added by default. Added these two lines:
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.3/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.8/dist/esm/index.js"
-pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.3-4/app/assets/javascripts/rails-ujs.esm.js"
