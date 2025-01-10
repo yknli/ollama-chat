64fc68264e80f7423992452054cbc8a1856e1d8e
yknli <augustus0613@hotmail.com>
2025-01-11 00:20:08 +0800

    feat: add google search api library

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem "thruster", "~> 0.1.10", require: false
 gem "ollama-ai", "~> 1.3"
 gem "dotenv", "~> 3.1"
 gem "haml-rails", "~> 2.1"
+gem "google-apis-customsearch_v1", "~> 0.23.0"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
---
+++ lib/services/google_api.rb
@@ -0,0 +1,22 @@
+require "google/apis/customsearch_v1"
+
+class GoogleApi
+
+  def self.search_web(query)
+    client = Google::Apis::CustomsearchV1::CustomSearchAPIService.new
+    client.key = ENV["GOOGLE_CUSTOM_SEARCH_API_KEY"]
+    response = client.list_cses(q: query, cx: ENV["GOOGLE_CUSTOM_SEARCH_ENGINE_ID"], num: 5)
+    results = []
+    if response.items.present?
+      response.items.each do |result|
+        results << {
+          title: result.title,
+          description: result.html_snippet,
+          link: CGI.unescape(result.link)
+        }
+      end
+    end
+    results
+  end
+
+end
