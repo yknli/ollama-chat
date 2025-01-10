4ec13680b31241bddfdd02eb2d2f34300bdaecd7
yknli <augustus0613@hotmail.com>
2025-01-11 01:08:51 +0800

    chore: move google custom search api credentials into credentials.yml

--- lib/services/google_api.rb
+++ lib/services/google_api.rb
@@ -2,9 +2,13 @@ require "google/apis/customsearch_v1"
 
 class GoogleApi
   def self.search_web(query)
+    custom_search_api_credentials = Rails.application.credentials.google.custom_search_api
+    api_key = custom_search_api_credentials.api_key
+    engine_id = custom_search_api_credentials.engine_id
+
     client = Google::Apis::CustomsearchV1::CustomSearchAPIService.new
-    client.key = ENV["GOOGLE_CUSTOM_SEARCH_API_KEY"]
-    response = client.list_cses(q: query, cx: ENV["GOOGLE_CUSTOM_SEARCH_ENGINE_ID"], num: 5)
+    client.key = api_key
+    response = client.list_cses(q: query, cx: engine_id, num: 5)
     results = []
     if response.items.present?
       response.items.each do |result|
